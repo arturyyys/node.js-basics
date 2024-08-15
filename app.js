923c09283955b69d02069ca9adcbc11fa3fc2fe3
Artūras <arturyyys@gmail.com>
2024-08-15 22:17:44 +0300

    assignment 1

--- app.js
+++ app.js
@@ -11,3 +11,45 @@ server.listen(3000);
 // localhost:3000/
 
 // quit running Node.js server pressing CTRL + C
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//     const method = req.method;
+
+//     if ( url === '/'){
+//         res.write('<html>');
+//         res.write('<head><title>users</title></head>');
+//         res.write('<body>');
+//         res.write('<h4>Hi everyone, Stephen speaking!</h4>');
+//         res.write('<ul><li>User one</li><li>User two</li><li>User three</li><li>User four</li></ul>')
+//         res.write('<form action="/create-user" method="POST"><input type="text" name="username"/><button>Submit</button></form>');
+//         res.write('</body>')
+//         res.write('</body>');
+//         return res.end();
+//     }
+
+//     if (url === '/create-user' && method === 'POST'){
+//         const body = [];
+
+//         req.on('data', (chunk) => {
+//             body.push(chunk);
+//         });
+
+//         return req.on('end', () => {
+//             const parsedBody = Buffer.concat(body).toString();
+//             const message = parsedBody.split('=')[1];
+//             console.log(message);
+//             return res.end()
+//         })
+//     }
+
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end();
+// });
+
+// const port = 3000;
+// server.listen(port, () => {
+//     console.log(`Server is running at http://localhost:${port}`);
+// })
