2b084a35bee3b911c1f53386830d2068e57bc57f
Artūras <arturyyys@gmail.com>
2024-08-17 21:30:54 +0300

    fixed

--- routes.js
+++ routes.js
@@ -21,7 +21,7 @@ const requestHandler = (req, res) => {
     });
     return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[0]; // debug message
+      const message = parsedBody.split("=")[1]; // debug message
       fs.writeFile("message.txt", message, (err) => {
         res.statusCode = 302;
         res.setHeader("Location", "/");
